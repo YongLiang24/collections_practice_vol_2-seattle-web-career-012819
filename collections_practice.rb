bf5f9c3a9586e29867645b46a828de9baa3a5e43
yong liang <yongliang24@gmail.com>
2019-01-21 23:11:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,6 @@
 # your code goes here
 def begins_with_r(array)
-  array.all? do |str|#he all? method returns true if the block never returns false or nil
+  array.all? do |str|#the all? method returns true if the block never returns false or nil
     str[0] == 'r'
   end
 end
